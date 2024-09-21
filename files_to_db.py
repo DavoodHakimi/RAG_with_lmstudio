f6db19359cccbb643c1d4b9a7d1bcf4bad4d1b4e
Davood <hakimi.davood.80@gmail.com>
2024-09-21 12:37:35 +0330

    minor changes

--- files_to_db.py
+++ files_to_db.py
@@ -5,35 +5,43 @@ import chromadb
 import pypdf
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.embeddings import FakeEmbeddings
+
+
+TEXT_EMBEDDING_MODEL = "BAAI/bge-large-zh" # Or you can use "BAAI/bge-large-en-v1.5" instead
+VECTOR_DB_NAME = "AI_llm"
+VECTOR_DB_PATH = "./DB/"
+CHUNK_SIZE = 1000
+OVERLAP = 10
 
 docs_path="./Docs/"
-
 file_names = [f for f in listdir(docs_path) if isfile(join(docs_path, f))]
 
-chroma_client = chromadb.PersistentClient(path="./DB/")
-collection=chroma_client.get_or_create_collection(name="AI_llm")
+chroma_client = chromadb.PersistentClient(path = VECTOR_DB_PATH)
+collection=chroma_client.get_or_create_collection(name = VECTOR_DB_NAME)
 existing_ids = set(collection.get(ids=None)["ids"]) # reading the doc ids to prevent from writing duplicate docs
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=10,
-    length_function=len,
-    is_separator_regex=False,
+    chunk_size = CHUNK_SIZE,
+    chunk_overlap = OVERLAP,
+    length_function = len,
+    is_separator_regex = False,
 )
-# text_embedding=HuggingFaceEmbeddings(model_name="BAAI/bge-large-en-v1.5")
-text_embedding=HuggingFaceEmbeddings(model_name="BAAI/bge-large-zh")
 
-for filename in file_names[1:]:
+text_embedding=HuggingFaceEmbeddings(
+    model_name = TEXT_EMBEDDING_MODEL
+)
+
+def write_2_DB(filename):
     print("Trying to Read ",filename)
     doc=pypdf.PdfReader(docs_path + filename)
+
     print("\n number of pages:",len(doc.pages))
     doc_text=""
     for page_num in tqdm(range(len(doc.pages)), desc="Adding Pages"):
         doc_text += doc.pages[page_num].extract_text()
     print("\n",filename, " Loaded Succesfully!!")
     
-    print("\n Chunking Started")
+    print("\n Chunking Started ...")
     chunks=text_splitter.split_text(doc_text)
     print("\n Chunking Done!")
 
@@ -43,10 +51,9 @@ for filename in file_names[1:]:
         doc_id=f"{filename}_{i}"
 
         if doc_id in existing_ids:
-            # print(f"ID {doc_id} already exists, skipping...")
             continue
 
-        # Chroma can't add big sized bachtes to Vector DB, so i did this to handle adding big files to DB
+        # Chroma can't add big size bachtes to Vector DB, so i did this to handle adding large files to DB
         documents_list = []
         embeddings_list = []
         ids_list = []
@@ -61,3 +68,7 @@ for filename in file_names[1:]:
             documents=documents_list,
             ids=ids_list
         )
+
+
+for filename in file_names:
+    write_2_DB(filename)
